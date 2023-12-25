b91401b5434043cb1b2f659e58e0c42189f95036
TheoRandstad <154253585+TheoRandstad@users.noreply.github.com>
2023-12-25 17:37:02 +0200

    Single statements Update with multiple statements to show the --amend usage

---
+++ statements.py
@@ -0,0 +1,21 @@
+# statement 1
+print('Hello')
+
+# statement 2
+x = 20
+
+# statement 3
+print(x)
+
+# two statements in a single
+l = 10; b = 5
+
+addition = 10 + 20 + \
+           30 + 40 + \
+           50 + 60 + 70
+print(addition)
+
+addition = (10 + 20 +
+            30 + 40 +
+            50 + 60 + 70)
+print(addition)
